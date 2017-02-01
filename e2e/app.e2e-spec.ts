cd4b86f628f5cc455f6c67550ea9fbfd5457c928
Darren Luvaas <darren@luvaas.com>
2017-01-31 16:49:29 -0800

    fixing tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('mean-cli-start App', function() {
 
 	it('should display message saying app works', () => {
 		page.navigateTo();
-		expect(page.getParagraphText()).toEqual('app works!');
+		expect(page.getParagraphText()).toContain('App works!');
 	});
 });
