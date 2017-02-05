8df62fef5f37f126c60cfcf8a935104f9e82937c
Darren Luvaas <darren@luvaas.com>
2017-02-04 23:13:18 -0800

    add examples of http with/without jwt in header

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,12 +1,13 @@
 // The user service contains a standard set of CRUD methods for managing users.
 
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { AuthHttp, JwtHelper } from 'angular2-jwt';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class UserService {
-	constructor(private http: Http, private authHttp: AuthHttp) { }
+	constructor(private http: Http, private authHttp: AuthHttp, private authService: AuthService) { }
 
 	// NOTE: The user routes to the API server are all protected.  Only admins should have access.
 	// Therefore, we are using the authHttp method instead of the usual Http, which will take care of
@@ -37,9 +38,26 @@ export class UserService {
 			.map((response: Response) => response.json());
 	}
 
-	// NOTE: This as an example of an unprotected route with no JWT passed
+	// NOTE: This as an example of an unprotected route WITHOUT a JWT passed in the header
 	// create(data: any) {
 	// 	return this.http.post('/api/users', data)
 	// 		.map((response: Response) => response.json());
 	// }
+
+	// NOTE: This as an example of an unprotected route WITH a JWT passed in the header
+	// create(data: any) {
+	// 	return this.http.post('/api/users', data, this.jwt())
+	// 		.map((response: Response) => response.json());
+	// }
+
+	/***** Utilities *****/
+
+	private jwt() {
+		// Add JWT token to all request headers
+		let token = this.authService.getToken();
+		if (token) {
+			let headers = new Headers({ 'Authorization': 'Bearer ' + token });
+			return new RequestOptions({ headers: headers });
+		}
+	}
 }
