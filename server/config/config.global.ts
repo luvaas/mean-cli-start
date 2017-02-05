f348cddc89d54626d73a63454e3ab3fc0736f18e
Darren Luvaas <darren@luvaas.com>
2017-02-04 20:02:28 -0800

    make expiration time easier to read

--- server/config/config.global.ts
+++ server/config/config.global.ts
@@ -1,6 +1,6 @@
 var config: any = {
 	saltRounds : 12,
-	tokenExpiresIn : 1440 * 60 // The JWT token expires in 24 hours (1440 minutes * 60 seconds = 24 hours)
+	tokenExpiresIn : 24 * (60 * 60), // The JWT token expires in 24 hours
 };
 
 module.exports = config;
